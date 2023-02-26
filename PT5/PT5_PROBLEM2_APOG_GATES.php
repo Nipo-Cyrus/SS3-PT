99c5d8bc88efcf626191d94eea797ac5f5b09612
Nipo Cyrus <118452258+Nipo-Cyrus@users.noreply.github.com>
2023-02-26 09:51:34 +0000

    Modified PT5

---
+++ PT5/PT5_PROBLEM2_APOG_GATES.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Conditional Statements </title>
+</head>
+<body>
+  
+<?php
+$subtotal = 0; 
+$value =  6;  
+
+//Enter an integer between 1 and 10
+for ($i = 1; $subtotal <= 100; $i++) {
+   $subtotal = $subtotal + $value ; 
+    echo "Subtotal: " . $subtotal . "<br>";
+    if ($subtotal > 100) {
+        echo "Previous subtotal: " . ($subtotal - $value) ;
+    }
+}
+echo "<br>";
+echo "<br>";
+?>
+
+</body>
+</html> 
