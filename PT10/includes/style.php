d8c43e152533153d7c1ce82ac364d48d3864f31f
Nipo Cyrus <118452258+Nipo-Cyrus@users.noreply.github.com>
2023-05-10 03:01:35 +0000

    Status: Finished

---
+++ PT10/includes/style.php
@@ -0,0 +1,253 @@
+<style>
+  #order {
+    padding: 14px 13px;
+    justify-content: center;
+    cursor: pointer;
+    border-radius: 5px;
+    background-color: #81849f;
+    margin-top: -700px;
+  }
+  #user {
+   padding: 14px 13px;
+    float: right;}
+
+  #right
+  {
+    float: right;
+  }
+  #styleN{
+    position: relative;
+   animation: mymove 20s infinite;
+   
+}
+
+@keyframes mymove {
+  from {left: 0px;}
+  to {left: 100%;}
+}
+  
+  
+  .sddw {
+    color: red;
+  }
+ 
+  
+     ul {
+          list-style-type: none;
+          margin: 0;
+          padding: 0;
+          overflow: hidden;
+          background-color: #81849f;
+        }
+        
+        li {
+          float: left;
+        }
+        
+        li a {
+          display: block;
+          color: white;
+          text-align: center;
+          padding: 14px 16px;
+          text-decoration: none;
+        }
+        
+        li a:hover {
+          background-color: #9fe2bf;
+        }
+
+      body {
+        margin: 0;
+        font-family: "Times New Roman", Times, serif;
+        color: white;
+      }
+     
+            
+        hr {
+            width: 100%;
+        }
+        .sext {
+            margin-left: 900px;
+        }
+   
+        p { font-size: 20px;}
+     
+    
+
+input[type="checkbox"],
+input[type="number"] {
+ margin: 0;
+ vertical-align: middle;
+}
+
+
+td[align="right"] {
+ font-weight: bold;
+ padding: 20px;
+ border-top: 1px solid #ccc;
+ text-align: right;
+}
+#td{
+  border-top: 1px solid #ccc;
+  text-align: center;
+}
+
+
+select {
+ padding: 10px;
+ border-radius: 5px;
+ border: none;
+ width: 50%;
+ font-size: 18px;
+ margin: 20px 0;
+ background-color: #f8f8f8;
+ color: #333;
+ border: 1px solid #ccc;
+ appearance: none;
+}
+
+
+button[type="submit"] {
+  padding: 14px ;
+ margin: 20px 0;
+    justify-content: center;
+    cursor: pointer;
+    border-radius: 5px;
+    background-color: #81849f;
+}
+#ass{
+ 
+    cursor: pointer;
+    margin-left: 60px;
+    margin-right: 20px;
+    
+    
+}
+
+
+button[type="submit"]:hover {background-color: yellow;}
+ 
+table {
+ border-collapse: collapse;
+ width: 50%;
+ max-width: 500px;
+ margin-top: 70px;
+ background-color: black;
+ border-radius: 5px;
+ font-size: 18px;
+ line-height: 1.5;
+ overflow: hidden;
+ box-shadow: 0 2px 10px #eabeb4;
+}
+
+#at{
+border-collapse: collapse;
+ width: 50%;
+ max-width: 300px;
+ margin-top: -60px;
+ background-color: black;
+ border-radius: 5px;
+ font-size: 18px;
+ line-height: 1.5;
+ overflow: hidden;
+ box-shadow: 0 2px 10px #eabeb4;
+
+}
+
+
+th {
+ background-color: #f5deb3;
+ font-weight: bold;
+ text-align: left;
+ padding: 20px;
+ color: #333;
+ text-transform: uppercase;
+ letter-spacing: 1px;
+ border-bottom: 1px solid #ccc;
+}
+
+
+tr {
+ border-bottom: 1px solid #ccc;
+}
+
+
+td:first-child {
+ font-weight: bold;
+ padding: 20px;
+}
+
+body {background-color: black;}      
+
+hr { width: 80%;}
+input[type="checkbox"] {margin-right: 10px;}
+input[type="number"] { width: 50px; }
+#style0 { width: 300px;  }  
+
+form {
+ display: flex;
+ flex-wrap: wrap;
+ justify-content: center;
+ align-items: center;
+ min-height: 100vh;
+}
+fieldset {
+ display: flex;
+ flex-direction: column;
+ justify-content: center;
+ align-items: center;
+ min-width: 350px;
+ width: 80%;
+}
+legend {
+ text-align: center;
+}
+label {
+ display: flex;
+ align-items: center;
+ justify-content: space-between;
+ width: 300px;
+ margin-bottom: 10px;
+}
+        
+#total-amount {           
+ margin-top: 20px; 
+ font-size: 1.2rem;
+}  
+
+#table3 {
+ color: red;
+ font-size: 1.2rem;
+ font-weight: bold;
+ text-align: center;
+ padding: 20px;
+ border-top: 1px solid #ccc;
+ text-align: right;
+}
+
+.receipt {
+ margin-left: 60px;
+}
+.receipt1 {
+ margin-left: 86px;
+} 
+.receipt2 {
+ margin-left: 113px;
+}
+.receipt3 {
+ margin-left: 24px;
+}
+.receipt4 {
+ margin-left: 75px;
+}
+
+.tet{
+        margin-left: 500px;
+        text-decoration: none;
+        color: aqua;
+        font-size: 20px;
+        margin-top: -476px;
+        position: absolute;
+
+      }
+</style>
---
+++ PT10/page2.php
@@ -0,0 +1,44 @@
+<?php 
+      session_name('store');
+      session_start();
+      include('includes/style.php');
+  ?>
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+  <ul>
+  
+  <?php 
+    $user = $_SESSION['username'];
+      if (empty($user)){
+       echo "<script> window.location.href='front.php'</script>";
+        }
+        echo '<li id = "user"><strong> User: '.  $user . '</strong></li>'; 
+   ?>
+      <li id = "right"><a href="front.php">Log Out</a></li>
+  </ul>
+  <body>
+  <?php $user = $_SESSION['username']; if (empty($user)){ echo "<script> window.location.href='front.php'</script>"; } 
+  $ordertype = $_SESSION["ordertype"]; 
+  $receipt = $_SESSION["receipt"]; 
+  $totalAmount = $_SESSION["totalAmount"]; 
+
+  echo '<center>'; 
+  echo '<h1 >Hyphinx Cafe Receipt</h1>'; 
+  echo $receipt; 
+  echo '<tr><td colspan="3" align="right">Order Type: </td><td align="center">' . $ordertype . '</td></tr>';  
+  echo '<tr><td colspan="3" align="right">Total Amount: </td><td align="center">$' . $totalAmount . '</td></tr> </table>';
+  echo '<h2>Thank you for your Purchase, &nbsp;<i class="sddw">' . $user . '</i></h2>'; 
+  echo '<form method="" action="page1.php">
+  <button type="tp" id = "order">Order Again?</button>
+</form>'
+  ?>
+
+
+<body> 
+</html>
